d0895503f1c091cb0d47ba26373b7e07d93f122b
Dandara <dands20melo@gmail.com>
2023-04-10 21:47:09 -0300

    Continuação da atividade

--- atvJs/code.js
+++ atvJs/code.js
@@ -1,4 +1,5 @@
-//Revisão
+//lista 1 
+
 //Exercício 1
 
 const mostra = mostrar()
@@ -12,7 +13,7 @@ if (mostra<=18){
     
 //Exercício 2
 
-function for(){
+function para(){
     idade = prompt("Digite um valor")
     console.log(idade)
     for(let i = 0;i<=n;i++){
@@ -22,4 +23,40 @@ function for(){
             console.log("Dado não encontrado!")
         }
     }
+}
+
+//Exercício 3
+
+function enquanto(){
+    let i = 0
+    while(1<10){
+        idade = prompt("Digite um valor")
+        console.log(idade)
+            if (i == n){
+                console.log("Você tem esa idade:" + idade)
+            } else {
+                console.log("Dado não encontrado!")
+            }
+    }
+}
+
+//Exercício 4
+/* */
+
+//Exercício 5
+
+function repetidor(numero){
+
+    switch(numero){
+        case 1: console.log("Gosto de anime Naruto" + numero)
+        break
+        case 2: console.log("One Punch é muito bom" + numero)
+        break
+        case 3: console.log("A série Supernatural é muito boa" + numero)
+        break
+        case 4: console.log("Vikings com Ragnar era o melho" + numero)
+        break
+        default: console.log("você assiste algo que não está na lista")
+    }
+
 }
