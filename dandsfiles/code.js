78432a45270979356897387c0f731ff2e1059846
Dandara <dands20melo@gmail.com>
2023-04-20 12:29:26 -0300

    atv de node

---
+++ dandsfiles/code.js
@@ -0,0 +1,10 @@
+function teste(){
+    var dados = [
+        {id: 1, login: "Will", senha: "1315"},
+        {id: 2, login: "Black", senha: "1111"},
+        {id: 3, login: "Sandra", senha: "2020"}
+    ]
+    
+    console.log(dados[2].id + " " + dados[2].login + " " + dados[2].senha)
+    window.alert(dados[2].id + " " + dados[2].login + " " + dados[2].senha)
+}
