d4975fa12973b44ea57777be9e573d2b67907a01
Dandara <dands20melo@gmail.com>
2023-04-20 12:59:46 -0300

    Alterações

--- dandsfiles/code.js
+++ dandsfiles/code.js
@@ -5,6 +5,30 @@ function teste(){
         {id: 3, login: "Sandra", senha: "2020"}
     ]
     
-    console.log(dados[2].id + " " + dados[2].login + " " + dados[2].senha)
-    window.alert(dados[2].id + " " + dados[2].login + " " + dados[2].senha)
+    return dados
+
+    //console.log(dados[2].id + " " + dados[2].login + " " + dados[2].senha)
+    //window.alert(dados[2].id + " " + dados[2].login + " " + dados[2].senha)
+}
+
+function logon(usuario, senha){
+
+    const conj = teste()
+
+    //console.log(conj[0].id)
+    //alert(conj[0].id)
+
+    if(usuario == conj[0].login){
+        window.alert("Você está logado!")
+    } else{
+        window.alert("Erro!")
+    }
+}
+
+function logar(){
+
+    let use = prompt("Qual é seu usuário? ")
+    let pas = prompt("Qual é sua senha? ")
+
+    logon(user, password)
 }
